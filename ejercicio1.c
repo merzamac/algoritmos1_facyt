be2afe2845a2459004b8bb456be1eeda330732b1
merzamac <moisesmerza@gmail.com>
2022-05-11 21:50:30 -0400

    ejrcicio listo

---
+++ ejercicio1.c
@@ -0,0 +1,20 @@
+/*convertir un numero de decimal a binario*/
+#include<stdio.h>
+
+int abinario(int num){
+    int resultado;
+
+    if (num < 2)
+    {
+        resultado = num;
+    }
+    else
+    {
+        resultado = num%2 + (10*abinario(num/2));
+    }
+    return resultado;
+}
+int main(){
+    printf("%i",abinario(7));
+    return 0;
+}
