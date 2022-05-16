5e858116dcabfd9bbceceaafa3c3b92820f424a3
merzamac <moisesmerza@gmail.com>
2022-05-16 19:23:29 -0400

    ejercicio2

---
+++ ejercicio2.c
@@ -0,0 +1,33 @@
+/*es una palabra palindromo*/
+#include<stdio.h>
+#include<stdbool.h>
+#include<string.h>
+
+bool espalindromo(char cadena[],int n,int in){
+    bool resultado;
+
+    if (in <= n){
+        if(cadena[n]==cadena[in]){
+            resultado = espalindromo(cadena,n-1,in+1);
+        }else{
+            resultado = false;
+        }
+    }
+    else 
+    {
+        resultado = true;
+    }
+    return resultado;
+}
+int main(){
+    bool resultado;
+    char cadena[]="aba"; //arepera,abba,hannah
+    if(espalindromo(cadena,strlen(cadena)-1,0)){
+        printf("lo es, %s ",cadena);
+    }else{
+        printf("no lo es, %s ",cadena);
+    }
+
+    
+    return 0;
+}
